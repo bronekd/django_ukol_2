bea4818847374cf99646dc3443aa7d0a89cb7ea9
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-07-08 23:04:06 +0200

    kod ještě nefunguje pokračovat detailem

--- book_library/book/views.py
+++ book_library/book/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
-def book(request):
+from .models import Book
+def book2(request):
     template = loader.get_template('book.html')
     return HttpResponse(template.render())
+
+def book(request):
+    my_books = Book.objects.all().values
+    template = loader.get_template('all_books.html')
+    context = {
+        'my_books': my_books,
+    }
+    return HttpResponse(template.render(context, request))
 
 
