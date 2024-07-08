41f0be7e3f636e4d74758963929941685f2a20a1
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-07-08 22:48:01 +0200

    přidaná data do databáze

--- book_library/book/models.py
+++ book_library/book/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Book(models.Model):
   namebook = models.CharField(max_length=255)
   autor = models.CharField(max_length=255)
+
+  def __str__(self):
+    return f"{self.namebook} {self.autor}"
